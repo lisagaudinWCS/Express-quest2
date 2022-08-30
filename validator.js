25a3e98da1db2c2081aa5449ae8b834f07eb31d7
lisagaudinWCS <lisagaudin.wcs@gmail.com>
2022-08-30 12:07:21 +0200

    express-validator challenge 04bis

--- validator.js
+++ validator.js
@@ -1,64 +1,39 @@
-const validateMovie = (req, res, next) => {
+const { body, validationResult } = require('express-validator');
 
-    const { title , director, year, color, duration } = req.body;
-    const errors = [];
+const validateMovie = [
+    body("title").isLength({ max : 255}),
+    body("director").isLength({ max : 255}),
+    body("year").isLength({ max : 255}),
+    body("color").isLength({ max : 255}),
 
-    if(title == null) {
-        errors.push({ field: "title", message: "This field is required" });
-    } else if (title.length >= 255) {
-            errors.push({ field : "title", message : "data shouldn't be more than 255"});
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            res.status(422).json({ validationErrors: errors.array() });
         }
-    if(director == null) {
-        errors.push({ field: "director", message: "This field is required" });
-    }
-    if(year == null) {
-        errors.push({ field: "year", message: "This field is required" });
-    }
-    if(color == null) {
-        errors.push({ field: "color", message: "This field is required" });
-    }
-    if(duration == null) {
-        errors.push({ field: "duration", message: "This field is required" });
-    }
-    if (errors.length) {
-        res.status(422).json({validationErrors: errors});
-    }
-    else {
-        next();
-    }
-};
+        else {
+            next();
+        }
+    },
+];
 
-const validateUser = (req, res, next) => {
+const validateUser = [
+  body("email").isEmail().isLength({ max : 255}),
+  body("firstname").isLength({ max: 255 }),
+  body("lastname").isLength({ max: 255 }),
+  body("city").isLength({ max : 255}),
+  body("language").isLength({ max : 255}),
+  (req, res, next) => {
+    const errors = validationResult(req);
 
-    const { firstname, lastname, email, city, language } = req.body;
-    const errors = [];
-
-    if (firstname == null) {
-        errors.push({ field : "firstname", message : "this field is required"});
+    if (!errors.isEmpty()) {
+      res.status(422).json({ validationErrors: errors.array() });
+    } else {
+      next();
     }
-    if (lastname == null) {
-        errors.push({ field : "lastname", message : "this field is required"});
-    }
-
-    const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
-
-    if (!emailRegex.test(email)) {
-        errors.push({ field : "email", message : "Invalid email"});
-    }
-    if (city == null) {
-        errors.push({ field : "city", message : "this field is required"});
-    }
-    if (language == null) {
-        errors.push({ field : "language", message : "this field is required"});
-    }
-    if (errors.length) {
-        res.status(422).json({ validationErrors : errors });
-    }
-    else {
-        next();
-    }
-};
-
+  },
+];
 
 
 module.exports = {
